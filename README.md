35a281ccb6013d4fed2068660c7f199b9f0fae0f
Chris Thomas <cepthomas@gmail.com>
2022-02-05 12:38:54 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+# Sublime Text plugin development playground. Nothing to see here, move along.
+
+## General Notes
+
+- In the code, `line` refers to editor lines and is 1-based. `row` refers to buffer contents as an array and is 0-based.
+- Project collections, variables, functions, etc use:
+    - `persisted` is the json compatible file format.
+    - `visual` is the way ST API handles elements.
+    - `internal` is the plugin format.
+- Commands can't end with `<underscore numeral>` e.g. `my_cmd_1` should be `stpt_cmd1`.
+- If you pass a dict as value in View.settings().set(name, value), it seems that the dict key must be a string.
+
+## Error Handling
+Because ST takes ownership of the python module loading and execution, it just dumps any load/parse and runtime exceptions
+to the console. This can be annoying because it means you have to have the console open pretty much all the time.
+First attempt was to hook the console stdout but it was not very cooperative. So now there are try/except around all the
+ST callback functions and this works to catch runtime errors and pop up a message box. Import/parse errors still go to the
+console so you have to keep an eye open there while developing but they should resolve quickly.
+
+
+## Event Handling
+There are some idiosyncrasies with ST event generation.
+
+### ViewEventListener
+Is instantiated once per view and:
+- `on_load()` is normally called when the file is loaded. However it is not called if ST startup shows previously opened files,
+  or if it is shown as a (single-click) preview.
+- `on_close()` is normally called when a view is closed but it does not appear to be consistent. Perhaps if ST is closed
+  without closing the views first?
+
+Why does it matter? Highlighting and signets persist their state per file and the application needs to hook the open/close
+events in order to do so. Because the two obvious events don't work as expected (by me at least), some
+less-than-beautiful hacks happen:
+- `on_activated()` is normally called when the view gets focus. This is reliable so is used instead of on_load(), along with
+  some stuff to track if it's been initialized.
+- `on_deactivated()` is used in place of `on_close()` to save the persistence file every time the view loses focus. Good enough.
+
+### EventListener
+Is instantiated once per window (ST instance):
+- `on_load_project()` doesn't fire on startup (last) project load.
+- `on_exit()` called once after the API has shut down, immediately before the plugin_host process exits.
+- `on_pre_close_window()` seems to work.
+
+### Global
+ST says `plugin_loaded()` fires only once for all instances of sublime. However you can add this to 
+each module and they all get called. Safest is to only use it once.
+
+
+## Module Loading
+ST doesn't load modules like plain python and can cause some surprises. The problem is that sbot_common
+gets reloaded but it appears to be a different module from the one linked to by the other modules.
+This makes handling globals difficult. Modules that are common cannot store meaningful state.
---
+++ sbot_dev.py
@@ -0,0 +1,137 @@
+import os
+import subprocess
+import sublime
+import sublime_plugin
+
+
+#-----------------------------------------------------------------------------------
+def plugin_loaded():
+    # print(">>> SbotDev plugin_loaded()")
+    pass
+
+
+#-----------------------------------------------------------------------------------
+def plugin_unloaded():
+    # print("SbotDev plugin_unloaded()")
+    pass
+
+
+#-----------------------------------------------------------------------------------
+class SbotTestPanelCommand(sublime_plugin.WindowCommand):
+    ''' Run a simple command in the project dir. '''
+
+    def run(self):
+        directions = ["north", "south", "east", "west"]
+
+        items = []
+        for l in directions:
+            items.append(sublime.QuickPanelItem(l, details=["<i>details</i>", "<b>more</b>"], annotation=f"look_{l}", kind=sublime.KIND_NAVIGATION))
+
+            # trigger - A unicode string of the text to match against the user's input.
+            # details - An optional unicode string, or list of unicode strings, containing limited inline HTML. Displayed below the trigger.
+            # annotation - An optional unicode string of a hint to draw to the right-hand side of the row.
+            # kind - An optional kind tuple – defaults to sublime.KIND_AMBIGUOUS.
+
+            # sublime.KIND_AMBIGUOUS When there source of the item is unknown – the default. Letter: none, theme class: kind_ambiguous
+            # sublime.KIND_KEYWORD When the item represents a keyword. Letter: k, theme class: kind_keyword
+            # sublime.KIND_TYPE When the item represents a data type, class, struct, interface, enum, trait, etc. Letter: t, theme class: kind_type
+            # sublime.KIND_FUNCTION When the item represents a function, method, constructor or subroutine. Letter: f, theme class: kind_function
+            # sublime.KIND_NAMESPACE When the item represents a namespace or module. Letter: a, theme class: kind_namespace
+            # sublime.KIND_NAVIGATION When the item represents a definition, label or section. Letter: n, theme class: kind_navigation
+            # sublime.KIND_MARKUP When the item represents a markup component, including HTML tags and CSS selectors. Letter: m, theme class: kind_markup
+            # sublime.KIND_VARIABLE When the item represents a variable, member, attribute, constant or parameter. Letter: v, theme class: kind_variable
+            # sublime.KIND_SNIPPET When the item contains a snippet. Letter: s, theme class: kind_snippet
+
+
+            # sublime.MONOSPACE_FONT - use a monospace font
+            # sublime.KEEP_OPEN_ON_FOCUS_LOST - keep the quick panel open if the window loses input focus
+            # sublime.WANT_EVENT - pass a second parameter to on_done, an event Dict
+
+        self.window.show_quick_panel(items, self.on_done,
+            flags=sublime.KEEP_OPEN_ON_FOCUS_LOST | sublime.MONOSPACE_FONT | sublime.MONOSPACE_FONT,
+            selected_index=2,
+            on_highlight=self.on_highlight,
+            placeholder="place-xxx")
+
+    def on_done(self, *args, **kwargs):
+        print(f"SEL:{args[0]}")
+        # print(f"on_done args:{args} kwargs:{kwargs}")
+
+    def on_highlight(self, *args, **kwargs):
+        print(f"HLT:{args[0]}")
+        # print(f"on_highlight args:{args} kwargs:{kwargs}")
+
+
+#-----------------------------------------------------------------------------------
+class SbotTestPanelInputCommand(sublime_plugin.WindowCommand):
+    ''' Run a simple command in the project dir. '''
+
+    def run(self):
+        # Bottom input area.
+        self.window.show_input_panel(self.window.extract_variables()['folder'] + '>', "", self.on_done, None, None)
+
+    def on_done(self, text):
+        cp = subprocess.run(text, cwd=self.window.extract_variables()['folder'], universal_newlines=True, check=True, capture_output=True, shell=True)
+        sout = cp.stdout
+        create_new_view(self.window, sout)
+
+
+#-----------------------------------------------------------------------------------
+class SbotTestPhantomsCommand(sublime_plugin.TextCommand):
+    # def __init__(self, view):
+    #     self.phantom_set = sublime.PhantomSet(self.view, "my_key")
+
+    def __init__(self, view):
+        super(SbotTestPhantomsCommand, self).__init__(view)
+        self.view = view
+        self.phantom_set = sublime.PhantomSet(self.view, "my_key")
+        self.count = 0
+
+    def run(self, edit):
+        # image = f"C:\\Users\\cepth\\AppData\\Roaming\\Sublime Text\\Packages\\SublimeBagOfTricks\\test\\files\\mark64.bmp"
+        image = os.path.join(sublime.packages_path(), "SbotDev", "felix.jpg")
+        img_html = '<img src="file://' + image + '" width="16" height="16">'
+
+        # Old way works too:
+        # self.view.erase_phantoms("test")
+        # self.view.erase_phantoms ("test")
+        # for sel in self.view.sel():
+        #     self.view.add_phantom ("test", sel, img_html, sublime.LAYOUT_BLOCK)
+
+        # Clean first. Note - phantoms need to be managed externally rather than instantiate each time cmd is loaded.
+        phantoms = []
+        self.phantom_set.update(phantoms)
+
+        html = f'<div>|image LAYOUT_INLINE at 200:210|{img_html}|</div>'
+        region = sublime.Region(200, 210)
+        phantom = sublime.Phantom(region, html, sublime.LAYOUT_INLINE)
+        phantoms.append(phantom)
+
+        html = f'<div>|image LAYOUT_BELOW at 400:410|{img_html}|</div>'
+        region = sublime.Region(400, 410)
+        phantom = sublime.Phantom(region, html, sublime.LAYOUT_BELOW)
+        phantoms.append(phantom)
+
+        html = f'<div>|image LAYOUT_BLOCK at 600:610|{img_html}|</div>'
+        region = sublime.Region(600, 610)
+        phantom = sublime.Phantom(region, html, sublime.LAYOUT_BLOCK)
+        phantoms.append(phantom)
+
+        href = "https://www.sublimetext.com/docs/api_reference.html"
+        href = "abcdef12345"
+
+        html = f'<div><a href="{href}">|href LAYOUT_BLOCK at 800:810|</a></div>'
+        region = sublime.Region(800, 810)
+        phantom = sublime.Phantom(region, html, sublime.LAYOUT_BLOCK, self.nav)
+        phantoms.append(phantom)
+
+        self.phantom_set.update(phantoms)
+
+        # sublime.LAYOUT_INLINE: Display in between the region and the point following.
+        # sublime.LAYOUT_BELOW: Display in space below the current line, left-aligned with the region.
+        # sublime.LAYOUT_BLOCK: Display in space below the current line, left-aligned with the beginning of the line.
+
+    def nav(self, href):
+        # on_navigate is an optional callback that should accept a single string parameter,
+        # that is the href attribute of the link clicked.
+        print(f"href:{href}")

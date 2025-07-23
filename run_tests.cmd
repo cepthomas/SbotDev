1b4d29a72576456a8401c38ecf46885f8724ce15
cepthomas <cepthomas@gmail.com>
2025-07-23 12:44:20 -0500

    WIP.

--- run_tests.cmd
+++ run_tests.cmd
@@ -2,17 +2,6 @@
 cls
 
 echo off
-TODO1 fix this
-del "C:\Users\cepth\AppData\Roaming\Sublime Text\Packages\User\_Test\trace.txt"
-
-rem :: Run unit tests from the command line. MY_PATH=%~dp0
-rem set PYTHONPATH=%~dp0test_files;%~dp0st_sim;
-rem :: Execute from the parent dir ST_PKGS="%APPDATA%\Sublime Text\Packages".
-rem pushd ..
-rem :: Execute all tests in a suite.  test_sbot.py  test_table.py  test_tracer.py
-rem python SbotDev\test_sbot.py
-rem :: Restore
-rem popd
 
 pushd ..\SbotDev\tests
 python -m unittest test_common test_tracer

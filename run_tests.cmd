81fe03df1ad8af9313c00ebad4366d81ce776afa
cepthomas <cepthomas@gmail.com>
2025-08-17 17:19:07 -0500

    Harmonize names.

--- run_tests.cmd
+++ run_tests.cmd
@@ -18,18 +18,3 @@ pushd ..\Notr\tests
 python -m unittest test_notr test_table
 popd
 
-pushd ..\SbotFormat\tests
-python -m unittest test_format
-popd
-
-rem pushd ..\SbotHighlight\tests
-rem python -m unittest test_highlight
-rem popd
-
-rem pushd ..\SbotRender\tests
-rem python -m unittest test_render
-rem popd
-
-rem pushd ..\SbotSignet\tests
-rem python -m unittest test_signet
-rem popd

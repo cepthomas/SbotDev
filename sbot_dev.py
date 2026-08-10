563402c99a05cd7cb1cd18385c56b1f7fcc75e9e
cepthomas <cepthomas@gmail.com>
2026-08-10 16:48:32 -0500

    WIP.

--- sbot_dev.py
+++ sbot_dev.py
@@ -14,6 +14,7 @@ import sublime_plugin
 from . import sbot_common as sc  # normal import - like ST loads
 
 
+
 #----------------- TODO1 Setup for running pbot_pdb in this file ---------------------
 # Pick one:
 #  - Copy pbot_pdb.py to this dir and edit to taste.
@@ -21,10 +22,7 @@ from . import sbot_common as sc  # normal import - like ST loads
 #  - Clone PyBagOfTricks and add its path to sys.path, something like this:
 # pbot_path = R'C:\Dev\Libs\PyBagOfTricks'
 # if pbot_path not in sys.path: sys.path.insert(0, pbot_path)
-
 # import pbot_pdb
-# Benign reload in case it's edited.
-# importlib.reload(pbot_pdb)
 
 
 #-----------------------------------------------------------------------------------
@@ -111,7 +109,7 @@ class SbotDebugCommand(sublime_plugin.TextCommand):
         # https://docs.python.org/3/library/pdb.html
 
 
-        breakpoint()
+#>>>        breakpoint()
 
 
         # Blow stuff up. Force unhandled exception.
@@ -341,14 +339,14 @@ class RunPdbCommand(sublime_plugin.TextCommand):
 
     def function1(self, arg):
         # Set a breakpoint here then step through and examine the code.
-#        pbot_pdb.breakpoint()
+#>>>        pbot_pdb.breakpoint()
         return self.function2(len(arg))
 
     def run(self, edit):
         del edit
 
         # Benign reload in case of edited.
- #       importlib.reload(pbot_pdb)
+        # importlib.reload(pbot_pdb)
 
         # Run some fake code.
         self.function1('ABCD')
--- tests/test_read_bin_file.py
+++ tests/test_read_bin_file.py
@@ -4,6 +4,8 @@ import time
 # from pathlib import Path
 from functools import partial
 
+# Todo1 make this a real unittesting
+
 # https://stackoverflow.com/questions/1035340/reading-binary-file-and-looping-over-each-byte
 
 BUFF_SIZE = 4096

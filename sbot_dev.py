db106766a2669d265734a6f1c0d5bd7a70e01556
cepthomas <cepthomas@gmail.com>
2026-08-11 15:17:08 -0500

    WIP.

--- sbot_dev.py
+++ sbot_dev.py
@@ -18,7 +18,6 @@ from . import sbot_common as sc  # normal import - like ST loads
 #----------------- TODO1 Setup for running pbot_pdb in this file ---------------------
 # Pick one:
 #  - Copy pbot_pdb.py to this dir and edit to taste.
-
 #  - Clone PyBagOfTricks and add its path to sys.path, something like this:
 # pbot_path = R'C:\Dev\Libs\PyBagOfTricks'
 # if pbot_path not in sys.path: sys.path.insert(0, pbot_path)
